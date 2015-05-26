5df5dea3be108f0ceac7ade54dfc9e173d73322b
KajTorvaldGrey <kajtorvaldgrey@googlemail.com>
2015-05-26 16:04:25 +0200

    first commit \o/

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author janes.thomas
+ */
+public class Main {
+    
+        static TestController controller;
+
+    /**
+     * Diese Klasse wird nur dazu benutzt alle nötigen
+     * Komponenten zu Initialisieren und die erste
+     * View anzuzeigen
+     */
+    public static void main(String [] args){
+        controller = new TestController();
+
+        controller.showView();
+    }
+}    
